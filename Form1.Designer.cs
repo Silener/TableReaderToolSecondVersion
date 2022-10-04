25d9bdfbdca3a8d3e5021586a8c558b6724f668d
Evelin Velikov <43967421+Silener@users.noreply.github.com>
2022-10-04 14:13:12 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace CryptoLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(566, 264);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Run Log";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 245);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 271);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(233, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(15, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(660, 210);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Log";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Data Source";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Database";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(63, 19);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(81, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(324, 245);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(185, 46);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Секунди, през които се извлича Log";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 303);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Crypto Log";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CryptoLog
+{
+    public partial class Form1 : Form
+    {
+        public static bool isChecked;
+
+        public static string connectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ListView.CheckForIllegalCrossThreadCalls = false;
+
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("Message", 500);
+            listView1.Columns.Add("Date", 200);
+
+            CheckBox.CheckForIllegalCrossThreadCalls = false;
+
+            textBox3.Text = "5";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true && Convert.ToInt32(textBox3.Text) < 5)
+            {
+                MessageBox.Show("Времето за извличане не може да е по-малко от 5 секунди");
+                checkBox1.Checked = false;
+                return;
+            }
+
+            if (checkBox1.Checked == true)
+            {
+                string database = textBox1.Text;
+                string catalog = textBox2.Text;
+                connectionString = String.Format(@"Data Source={0}; Initial Catalog={1};User ID=sa;Password=massive", database, catalog);
+
+                Thread childThread = new Thread(RunLog);
+                childThread.Start();
+
+                isChecked = true;
+
+            }
+            else
+            {
+                isChecked = false;
+            }
+        }
+
+        private void RunLog()
+        {
+            for (; ; )
+            {
+                
+                Thread.Sleep(Convert.ToInt32(textBox3.Text) * 1000);
+
+                if (Form1.isChecked)
+                {
+                    listView1.Items.Clear();
+
+                    Log log = new Log();
+
+                    using (SqlConnection myConnection = new SqlConnection(Form1.connectionString))
+                    {
+                        string oString = "Select TOP 50 message, date_msg from log_system with(nolock) where message like '%CryptoTrace%' order by id desc";
+
+                        SqlCommand oCmd = new SqlCommand(oString, myConnection);
+                        oCmd.CommandTimeout = 1000000;
+
+                        try
+                        {
+                            myConnection.Open();
+                        }
+                        catch (SqlException)
+                        {
+                            checkBox1.Checked = false;
+                            return;
+                        }
+                        
+                        using (SqlDataReader oReader = oCmd.ExecuteReader())
+                        {
+                            while (oReader.Read())
+                            {
+                                log.message = oReader["message"].ToString();
+                                log.date = oReader["date_msg"].ToString();
+
+                                string[] row = { log.message, log.date };
+
+                                ListViewItem item = new ListViewItem(row);
+
+                                listView1.Items.Add(item);
+                            }
+
+                            myConnection.Close();
+                        }
+                       
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    public class Log
+    {
+        public string message;
+
+        public string date;
+    }
+}
